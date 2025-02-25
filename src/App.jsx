a4f8e35281ee5f27b99d7850b4d621faa429c1dd
Abdul Rafay <rajpootrafay96@gmail.com>
2025-02-25 15:25:22 +0500

    routte will changes whwne we click on /login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AllItemsCalling from './CallingAlliItems/AllItemsCalling';
+
 import Login from './Login/Login';
 import ForgetPassword from './ForgetPassword/ForgetPassword';
 
 function App() {
   return (
     <Router>
+      
       <Routes>
         <Route path="/Login" element={<Login />} />
-        <Route path="/items" element={<AllItemsCalling />} />
+  
         <Route path="/forget-password" element={<ForgetPassword/>} />
 
     
